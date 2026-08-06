f9ae25c9f359125e4e4d511f191af7827ef11d6d
Ereny Habib <erenyhabib4@gmail.com>
2026-08-06 20:29:44 +0300

    project

---
+++ src/Track _Ai/python_for_ML/Session_4/Code/chatbot-project/chat.py
@@ -0,0 +1,13 @@
+from responce import get_response
+def chatbot():
+    print("Chatbot: Hi! How can I assist you today?")
+   
+    while True:
+        user_input = input("User: ").lower()
+        response = get_response(user_input)
+        print("Chatbot:", response)
+       
+        if user_input == "goodbye":
+            break
+ 
+# Run the chatbot
